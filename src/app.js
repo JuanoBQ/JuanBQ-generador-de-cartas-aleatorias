30fed794f37381f9999f3eb5215b18fffc97f9bc
Juan Jose Bravo Quiroga <juanjo.bravo3@gmail.com>
2025-02-04 12:27:26 +0000

    Generador de cartas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,50 @@
 import "bootstrap";
 import "./style.css";
 
+{/* <i class="fa-solid fa-chess-king"></i>
+<i class="fa-solid fa-chess-queen"></i> */}
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let paloArray = ["♦", "♥", "♠", "♣"];
+let numberArray = [2, 3, 4, 5, 6, 7, 8, 9, 10, "K", "Q", "J", "As"]
+
+let randomPalo = Math.floor(Math.random() * paloArray.length);
+let randomNumber = Math.floor(Math.random() * numberArray.length);
+
+
+let palo = document.getElementsByClassName("palo");
+palo[0].innerHTML = paloArray[randomPalo];
+palo[1].innerHTML = paloArray[randomPalo];
+
+let numero = document.getElementsByClassName("numero");
+numero[0].innerHTML = numberArray[randomNumber];
+
+
+if (numberArray[randomNumber] === "K") numero[0].innerHTML = 'K<i class="fa-solid fa-chess-king"></i>';
+if (numberArray[randomNumber] === "Q") numero[0].innerHTML = 'Q<i class="fa-solid fa-chess-queen"></i>';
+if (paloArray[randomPalo] === "♥" || paloArray[randomPalo] === "♦") palo[0].style.color = "red";
+else  palo[0].style.color = "black";
+if (paloArray[randomPalo] === "♥" || paloArray[randomPalo] === "♦") palo[1].style.color = "red";
+else palo[1].style.color = "black";
+if (paloArray[randomPalo] === "♥" || paloArray[randomPalo] === "♦") numero[0].style.color = "red";
+else numero[0].style.color = "black";
+
+
+let refreshCard = document.getElementById("refresh")
+refreshCard.addEventListener("click", function () {
+  let randomPalo = Math.floor(Math.random() * paloArray.length);
+  let randomNumber = Math.floor(Math.random() * numberArray.length);
+
+  palo[0].innerHTML = paloArray[randomPalo];
+  palo[1].innerHTML = paloArray[randomPalo];
+  numero[0].innerHTML = numberArray[randomNumber];
+  if (numberArray[randomNumber] === "K") numero[0].innerHTML = 'K<i class="fa-solid fa-chess-king"></i>';
+  if (numberArray[randomNumber] === "Q") numero[0].innerHTML = 'Q<i class="fa-solid fa-chess-queen"></i>';
+  if (paloArray[randomPalo] === "♥" || paloArray[randomPalo] === "♦") palo[0].style.color = "red";
+  else  palo[0].style.color = "black";
+  if (paloArray[randomPalo] === "♥" || paloArray[randomPalo] === "♦") palo[1].style.color = "red";
+  else palo[1].style.color = "black";
+  if (paloArray[randomPalo] === "♥" || paloArray[randomPalo] === "♦") numero[0].style.color = "red";
+  else numero[0].style.color = "black";
+})
+
